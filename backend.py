c7a74108309b16f5095bb13ea0d90d0465da13b9
AnkurIngale <ingaleankur10@gmail.com>
2020-08-07 05:30:25 +0530

    Added sample objects to test table

--- backend.py
+++ backend.py
@@ -1,4 +1,11 @@
-from .flaskRun import getDriver
+import requests
+from flaskRun import getDriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
 def f_1mg(medicine):
     pass
--- flaskRun.py
+++ flaskRun.py
@@ -2,35 +2,48 @@ import flask
 from flask import Flask,request
 import selenium
 from selenium import webdriver
+import backend
 app = Flask(__name__)
 
 driver = None
 ANKUR_WEBDRIVER = "/Users/ankuringale/Desktop/chromedriver"
 MIHIR_WEBDRIVER = "/home/mihir/bin/chromedriver"
 
+sample_object = {
+	'link': '#',
+	'name': 'Medicine',
+	'price': 0
+}
+
+sample_data = [{
+	'apollo': sample_object,
+	'pharmeasy': sample_object,
+	'netmeds': sample_object,
+	'onemg': sample_object
+}] * 5
+
 @app.route('/')
-def index():
-	global driver
-	if not driver:
-		options = webdriver.ChromeOptions()
-		options.add_argument('--ignore-certificate-errors')
-		options.add_argument('--incognito')
-		options.add_argument('--headless')
-		driver = webdriver.Chrome(ANKUR_WEBDRIVER, chrome_options=options)
-	return flask.render_template('index.html')
+def index(data = None):
+	return flask.render_template('index.html', data=data)
 
 @app.route('/handle_data', methods = ['POST'])
 def handle_data():
-	projectpath = request.form['name']
-	driver.get('https://www.google.com/')
-	driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input').send_keys(projectpath)
-	driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]').submit()
-	print(driver.current_url)
-	return flask.render_template('index.html')
+	return flask.render_template('index.html', data=sample_data)
 
 def getDriver():
 	global driver
+	if not driver:
+		initDriver()
+	assert driver is not None
 	return driver
+
+def initDriver():
+	global driver
+	options = webdriver.ChromeOptions()
+	options.add_argument('--ignore-certificate-errors')
+	options.add_argument('--incognito')
+	options.add_argument('--headless')
+	driver = webdriver.Chrome(ANKUR_WEBDRIVER, chrome_options=options)
 
 if __name__ == '__main__':
 	app.debug=True

39c6d4d52ee285c997f9342934bb08c0ac928318
mihir-yadav <m.yadav.mihir@gmail.com>
2020-08-07 17:23:53 +0530

    Add existing file

--- backend.py
+++ backend.py
@@ -5,124 +5,254 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
+
+def get_img_src(medicine):
+	driver= getDriver()
+	driver.get('https://images.google.com');
+	driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input').send_keys(medicine)
+	driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input').send_keys(Keys.ENTER)
+	src = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img').get_attribute('src')
+	return src
+
+
 def f_1mg(medicine):
-    data = [
-        {
-            'link': '#',
-            'name': '',
-            'price': ''
-        }
-    ] * 5
-    return data
+	data = [
+		{
+			'link': '#',
+			'name': '',
+			'price': ''
+		}
+	] * 5
+	URL = "https://www.1mg.com/search/all?name=" + '%20'.join(medicine)
+	driver = getDriver()
+	driver.get(URL)
+	delay = 4
+	# return data
+	try:
+		# # driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/input')
+		# driver.find_element_by_class_name('jss34 _9Rsw_ undefined _2Saml')		
+		# driver.send_keys(' '.join(medicine))
+		# driver.send_keys(Keys.ENTER)
+		# driver.
+		print('searched\n')
+		# //*[@id="content"]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div
+		# //*[@id="content"]/div/div[3]/div/div/div[1]/div[1]/div/div[2]
+		print(driver.current_url)
+		try:
+			WebDriverWait(driver, delay).until(EC.presence_of_element_located(
+				(By.CLASS_NAME, 'row.style__grid-container___3OfcL')
+			))
+			try :
+				driver.find_element_by_class_name('style__horizontal-card___1Zwmt')
+				print('found1\n')
+				boxes = driver.find_elements_by_class_name('style__horizontal-card___1Zwmt')
+				print(len(boxes))
+				for i, box in enumerate(boxes):
+					data_obj = {
+						'link': '#',
+						'name': '',
+						'price': ''
+					}
+					data_obj['link'] = box.find_element_by_tag_name('a').get_attribute('href')
+					# data_obj['name'] = box.find_element_by_class_name('style__pro-title___3zxNC').text 
+					data_obj['name'] = box.find_element_by_class_name('style__product-description___1vPQe').text
+					# 
+					# +'\n'+ box.find_element_by_class_name('_36aef').text
+					# try :
+					data_obj['price'] = box.find_element_by_class_name('style__price-tag___B2csA').text
+					# except : 
+					# 	data_obj['price'] = box.find_element_by_class_name('nFRb7').text 
+			
+					data[i] = data_obj
+					print(i)
+			except:
+				try:
+					driver.find_element_by_class_name('style__product-box___3oEU6')
+					print('found2\n')
+					boxes = driver.find_elements_by_class_name('style__product-box___3oEU6')
+					print(len(boxes))
+					for i, box in enumerate(boxes):
+						data_obj = {
+							'link': '#',
+							'name': '',
+							'price': ''
+						}
+						data_obj['link'] = box.find_element_by_tag_name('a').get_attribute('href')
+						# data_obj['name'] = box.find_element_by_class_name('style__pro-title___3zxNC').text 
+						data_obj['name'] = box.find_element_by_class_name('style__product-description___zY35s').text
+						# 
+						# +'\n'+ box.find_element_by_class_name('_36aef').text
+						# try :
+						data_obj['price'] = box.find_element_by_class_name('style__price-tag___KzOkY').text
+						# except : 
+						# 	data_obj['price'] = box.find_element_by_class_name('nFRb7').text 
+				
+						data[i] = data_obj
+						print(i)
+				except:
+					pass
+		except:
+			pass
+	except:
+		pass
+	return data
+
 
 def f_pharmeasy(medicine):
-    data = [
-        {
-            'link': '#',
-            'name': '',
-            'price': ''
-        }
-    ] * 5
-    return data
+	data = [
+		{
+			'link': '#',
+			'name': '',
+			'price': ''
+		}
+	] * 5
+	URL = "https://pharmeasy.in/search/all?name=" + '+'.join(medicine)
+	driver = getDriver()
+	driver.get(URL)
+	delay = 4
+	# return data
+	try:
+		# # driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/input')
+		# driver.find_element_by_class_name('jss34 _9Rsw_ undefined _2Saml')		
+		# driver.send_keys(' '.join(medicine))
+		# driver.send_keys(Keys.ENTER)
+		# driver.
+		print('searched\n')
+		# //*[@id="content"]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div
+		# //*[@id="content"]/div/div[3]/div/div/div[1]/div[1]/div/div[2]
+		print(driver.current_url)
+		try:
+			WebDriverWait(driver, delay).until(EC.presence_of_element_located(
+				(By.CLASS_NAME, '_3zq4I')
+			))
+			print('found\n')
+			boxes = driver.find_elements_by_class_name('_1jald')
+			print(len(boxes))
+			for i, box in enumerate(boxes):
+				data_obj = {
+					'link': '#',
+					'name': '',
+					'price': ''
+				}
+				data_obj['link'] = box.find_element_by_class_name('_3o0NT._1NxW8').get_attribute('href')
+				data_obj['name'] = box.find_element_by_class_name('ooufh').text 
+				# +'\n'+ box.find_element_by_class_name('_36aef').text
+				try :
+					data_obj['price'] = box.find_element_by_class_name('_1_yM9').text[:-1]
+				except : 
+					data_obj['price'] = box.find_element_by_class_name('nFRb7').text 
+		
+				data[i] = data_obj
+				print(i)
+
+		except:
+			pass
+	except:
+		pass
+	return data
 
 def f_apollo(medicine):
-    URL = "https://www.apollopharmacy.in/"
-    driver = getDriver()
-    driver.get(URL)
-    delay = 4
+	pass
+	URL = "https://www.apollopharmacy.in/"
+	driver = getDriver()
+	driver.get(URL)
+	delay = 4
 
-    data = [
-        {
-            'link': '#',
-            'name': '',
-            'price': ''
-        }
-    ] * 5
+	data = [
+		{
+			'link': '#',
+			'name': '',
+			'price': ''
+		}
+	] * 5
+	# return data
+	try:
+		driver.find_element_by_id('search').send_keys(' '.join(medicine))
+		driver.find_element_by_id('search').send_keys(Keys.ENTER)
 
-    try:
-        driver.find_element_by_id('search').send_keys(' '.join(medicine))
-        driver.find_element_by_id('search').send_keys(Keys.ENTER)
+		try:
+			WebDriverWait(driver, delay).until(EC.presence_of_element_located(
+				(By.CLASS_NAME, 'header-results-and-footer')
+			))
 
-        try:
-            WebDriverWait(driver, delay).until(EC.presence_of_element_located(
-                (By.CLASS_NAME, 'header-results-and-footer')
-            ))
+			boxes = driver.find_elements_by_class_name('product-link')
+			# print(len(boxes))
+			for i, box in enumerate(boxes):
+				data_obj = {
+					'link': '#',
+					'name': '',
+					'price': ''
+				}
+				data_obj['link'] = box.get_attribute('href')
+				data_obj['name'] = box.find_element_by_class_name('product-name').text
+				data_obj['price'] = box.find_element_by_class_name('product-sale-price').text
+				data[i] = data_obj
+				# print(i)
 
-            boxes = driver.find_elements_by_class_name('product-link')
-
-            for i, box in enumerate(boxes):
-                data_obj = {
-                    'link': '#',
-                    'name': '',
-                    'price': ''
-                }
-                data_obj['link'] = box.get_attribute('href')
-                data_obj['name'] = box.find_element_by_class_name('product-name').text
-                data_obj['price'] = box.find_element_by_class_name('product-sale-price').text
-                data[i] = data_obj
-        except:
-            pass
-    except:
-        pass
-    
-    return data
+		except:
+			pass
+	except:
+		pass
+	
+	return data
 
 def f_netmeds(medicine):
-    URL = "https://www.netmeds.com/"
-    driver = getDriver()
-    driver.get(URL)
-    delay = 4
+	pass
+	URL = "https://www.netmeds.com/"
+	driver = getDriver()
+	driver.get(URL)
+	delay = 4
 
-    data = [
-        {
-            'link': '#',
-            'name': '',
-            'price': ''
-        }
-    ] * 5
+	data = [
+		{
+			'link': '#',
+			'name': '',
+			'price': ''
+		}
+	] * 5
+	# return data
+	try:
+		driver.find_element_by_id('search').send_keys(' '.join(medicine))
+		driver.find_element_by_id('search').send_keys(Keys.ENTER)
 
-    try:
-        driver.find_element_by_id('search').send_keys(' '.join(medicine))
-        driver.find_element_by_id('search').send_keys(Keys.ENTER)
+		try:
+			WebDriverWait(driver, delay).until(EC.presence_of_element_located(
+				(By.CLASS_NAME, 'sear-name')
+			))
 
-        try:
-            WebDriverWait(driver, delay).until(EC.presence_of_element_located(
-                (By.CLASS_NAME, 'sear-name')
-            ))
+			boxes = driver.find_elements_by_class_name('drug_list')
 
-            boxes = driver.find_elements_by_class_name('drug_list')
-
-            for i, box in enumerate(boxes):
-                data_obj = {
-                    'link': '#',
-                    'name': '',
-                    'price': ''
-                }
-                obj = box.find_element_by_class_name('drug_c').find_element_by_tag_name('a')
-                data_obj['link'] = obj.get_attribute('href')
-                data_obj['name'] = obj.find_element_by_class_name('info').text
-                data_obj['price'] = box.find_element_by_class_name('final-price').text
-                data[i] = data_obj
-        except:
-            pass
-    except:
-        pass
-    
-    print(data)
-    return data
+			for i, box in enumerate(boxes):
+				data_obj = {
+					'link': '#',
+					'name': '',
+					'price': ''
+				}
+				obj = box.find_element_by_class_name('drug_c').find_element_by_tag_name('a')
+				data_obj['link'] = obj.get_attribute('href')
+				data_obj['name'] = obj.find_element_by_class_name('info').text
+				data_obj['price'] = box.find_element_by_class_name('final-price').text
+				data[i] = data_obj
+		except:
+			pass
+	except:
+		pass
+	
+	print(data)
+	return data
 
 def compileData(medicine):
-    apollo = f_apollo(medicine)
-    pharmeasy = f_pharmeasy(medicine)
-    netmeds = f_netmeds(medicine)
-    onemg = f_1mg(medicine)
+	apollo = f_apollo(medicine)
+	pharmeasy = f_pharmeasy(medicine)
+	netmeds = f_netmeds(medicine)
+	onemg = f_1mg(medicine)
 
-    data = [0] * 5
-    for i in range(5):
-        data[i] = {
-            'apollo': apollo[i],
-            'pharmeasy': pharmeasy[i],
-            'netmeds': netmeds[i],
-            'onemg': onemg[i]
-        }
-    return data
+	data = [0] * 5
+	for i in range(5):
+		data[i] = {
+			'apollo': apollo[i],
+			'pharmeasy': pharmeasy[i],
+			'netmeds': netmeds[i],
+			'onemg': onemg[i]
+		}
+	return data
--- flaskRun.py
+++ flaskRun.py
@@ -3,76 +3,12 @@ from flask import Flask,request
 import selenium
 from selenium import webdriver
 import backend
-<<<<<<< HEAD
-from backend import *
-from selenium.common.exceptions import NoSuchElementException
-
-=======
->>>>>>> fbbb945bb9f512bbc16f466695688c8996c630dd
 app = Flask(__name__)
 
-driver = None
+driver = None 
 ANKUR_WEBDRIVER = "/Users/ankuringale/Desktop/chromedriver"
 MIHIR_WEBDRIVER = "/home/mihir/bin/chromedriver"
 
-<<<<<<< HEAD
-def find_apollo(medicine):
-	global driver
-	driver.get("https://www.apollopharmacy.in")
-	driver.find_element_by_xpath('//*[@id="search"]').send_keys(medicine)
-	driver.find_element_by_xpath('//*[@id="search"]').submit()
-	# print(driver.current_url)
-	for i in range(5):
-		pdt = driver.find_element_by_xpath('//*[@id="tagalys-namespace"]/div/div[2]/div[2]/div['+str(i+1)+']')
-		a = pdt.text.split('\n')
-		for x in a :
-			print(x)
-		link = pdt.get_attribute('href')
-		print(link)
-
-# //*[@id="tagalys-namespace"]/div/div[2]/div[2]/div[1]/a
-# //*[@id="tagalys-namespace"]/div/div[2]/div[2]/div[1]
-# //*[@id="tagalys-namespace"]/div/div[2]/div[2]/div[2]
-# 	# driver.get("https://www.apollopharmacy.in/tsearch?q="+medicine)
-	# try:
-	# 	unavailable = driver.find_element_by_class_name("no-products")
-	# 	return medicine + ' not found'
-	# except NoSuchElementException :
-	# 	pdt = driver.find_element_by_class_name("tagalys-product-tile")
-	# 	a = pdt.text.split('\n')
-	# 	return (a[0] + ' '+ a[1]).capitalize()
-
-
-
-@app.route('/')
-def index():
-	global driver
-	if not driver:
-		options = webdriver.ChromeOptions()
-		options.add_argument('--ignore-certificate-errors')
-		options.add_argument('--incognito')
-		options.add_argument('--headless')
-		driver = webdriver.Chrome(MIHIR_WEBDRIVER, chrome_options=options)
-	return flask.render_template('index.html')
-
-@app.route('/handle_data', methods = ['POST'])
-def handle_data():
-	global driver
-
-	projectpath = request.form.getlist('name')
-	medicine = projectpath[1] 
-	# driver.get("https://www.apollopharmacy.in/tsearch?q="+medicine)
-	# + '%20' + projectpath[1]
-	print(medicine)
-	# desc = f_netmeds(medicine)
-	desc = find_apollo(medicine)
-	print(desc)
-	# driver.get('https://www.google.com/')
-	# driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input').send_keys(projectpath)
-	# driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]').submit()
-	# print(driver.current_url)
-	return flask.render_template('index.html')
-=======
 sample_object = {
 	'link': '#',
 	'name': 'Medicine',
@@ -86,6 +22,7 @@ sample_data = [{
 	'onemg': sample_object
 }] * 5
 
+
 @app.route('/')
 def index(data = None):
 	return flask.render_template('index.html', data=data)
@@ -93,8 +30,8 @@ def index(data = None):
 @app.route('/handle_data', methods = ['POST'])
 def handle_data():
 	data = backend.compileData([request.form['type'], request.form['name']])
-	return flask.render_template('index.html', data=data)
->>>>>>> fbbb945bb9f512bbc16f466695688c8996c630dd
+	src = backend.get_img_src(' '.join(request.form['name']))
+	return flask.render_template('index.html', data=[data,src])
 
 def getDriver():
 	global driver
@@ -103,17 +40,14 @@ def getDriver():
 	assert driver is not None
 	return driver
 
-<<<<<<< HEAD
-=======
 def initDriver():
 	global driver
 	options = webdriver.ChromeOptions()
 	options.add_argument('--ignore-certificate-errors')
 	options.add_argument('--incognito')
 	options.add_argument('--headless')
-	driver = webdriver.Chrome(ANKUR_WEBDRIVER, chrome_options=options)
->>>>>>> fbbb945bb9f512bbc16f466695688c8996c630dd
+	driver = webdriver.Chrome(MIHIR_WEBDRIVER, chrome_options=options)
 
 if __name__ == '__main__':
 	app.debug=True
-	app.run()
+	app.run()    
